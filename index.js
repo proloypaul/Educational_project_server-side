d5e8a3a584331c6fb2511963fe375a4a3b8ec1c8
proloypaul <proloyp23@gmail.com>
2021-12-14 23:48:36 +0600

    connect mongodb

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+const { MongoClient } = require('mongodb');
 const express = require('express')
 const app = express()
-const port = 3800
+require('dotenv').config()
+const port = process.env.PORT || 3800
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.e3dsx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri)
+
+async function run(){
+    try{
+        await client.connect()
+
+
+    }finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send("Educational Server Side Running")
